6ae67eb6e083d9f55d212713717419e67cb67db5
aitemir <ajtemirulanbekuulu@gmail.com>
2022-02-04 17:08:00 +0600

    Firstcommit Jwt

---
+++ JWTWebAPITutorial/UserDto.cs
@@ -0,0 +1,9 @@
+namespace JWTWebAPITutorial
+{
+    public class UserDto  // user data transfer object
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
+    }
+}
